b416dead1839e8b877a312897132604404d64927
Isha Venikar <ishavenikar@Ishas-MacBook-Air.local>
2025-06-29 09:45:49 +0530

    Update login page UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,71 +7,77 @@ import {
   Heading,
   Input,
   Stack,
+  Text,
+  Link,
+  Flex,
 } from "@chakra-ui/react"
-import { FormLabel, FormControl } from "@chakra-ui/form-control";
-// import { useColorMode } from "@/components/ui/color-mode";
+import { FormLabel, FormControl } from "@chakra-ui/form-control"
+import { useState } from "react"
 
 export default function LoginPage() {
-  // const { colorMode } = useColorMode();
+  const bg = "#EADDCA"
+  const textColor = "#6B4F27"
 
-  const bg = '#EADDCA';
-  const textColor = '#6B4F27';
+  const [isLogin, setIsLogin] = useState(true)
+
+  const toggleMode = () => setIsLogin((prev) => !prev)
 
   return (
     <Box
       minH="100vh"
       bg={bg}
       display="flex"
-      alignItems="flex-start"
       justifyContent="center"
-      px={2}
-      pt={20}
+      alignItems="center"
+      px={4}
     >
       <Box
         bg="white"
-        p={5}
-        borderRadius="xl"
-        boxShadow="lg"
+        p={8}
+        borderRadius="2xl"
+        boxShadow="xl"
         w="full"
         maxW="sm"
       >
         <Stack as="form">
-          <Heading textAlign="center" color={textColor}>
-            Welcome
+          <Heading textAlign="center" color={textColor} size="lg">
+            {isLogin ? "Welcome Back 🐾" : "Join the Family 🤎"}
           </Heading>
 
           <FormControl id="email" isRequired>
             <FormLabel color={textColor}>Email</FormLabel>
-            <Input name="email" type="email" />
+            <Input name="email" type="email" color={textColor} />
           </FormControl>
 
           <FormControl id="password" isRequired>
             <FormLabel color={textColor}>Password</FormLabel>
-            <Input name="password" type="password" />
+            <Input name="password" type="password" color={textColor} />
           </FormControl>
 
-          <Stack>
-            <Button
-              type="submit"
-              formAction={login}
-              bg={textColor}
-              color="white"
-              _hover={{ color: textColor }}
-              w="full"
-            >
-              Log in
-            </Button>
-            <Button
-              type="submit"
-              formAction={signup}
-              variant="outline"
-              borderColor={textColor}
-              color={textColor}
-              w="full"
-            >
-              Sign up
-            </Button>
-          </Stack>
+          <Button
+            type="submit"
+            formAction={isLogin ? login : signup}
+            bg={textColor}
+            color="white"
+            _hover={{ bg: "#B28D5B" }}
+            w="full"
+          >
+            {isLogin ? "Log in" : "Sign up"}
+          </Button>
+
+          <Flex justify="center">
+            <Text fontSize="sm" color="gray.600">
+              {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+              <Link
+                onClick={toggleMode}
+                color={textColor}
+                fontWeight="medium"
+                cursor="pointer"
+              >
+                {isLogin ? "Sign up" : "Log in"}
+              </Link>
+            </Text>
+          </Flex>
         </Stack>
       </Box>
     </Box>
